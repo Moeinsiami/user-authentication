ce1e376e2aae5fa3cc55446390e9055013fc9ade
Moein Siami <moeins1216@gmail.com>
2024-09-01 11:19:20 -0700

    update email url

--- authenticate/users/urls.py
+++ authenticate/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import register_user, login_user, logout_user, dashboard, home
+from .views import register_user, login_user, logout_user, dashboard, home, update_email
 
 urlpatterns = [
     path('', home, name='home'), 
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'), 
-    path('dashboard/', dashboard, name='dashboard'), 
+    path('dashboard/', dashboard, name='dashboard'),
+    path('update-email/', update_email, name='update_email'),
 ]
